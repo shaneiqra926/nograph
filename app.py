b7777e027c86606fd386b962e634cb882e2101f4
shaneiqra926 <58846372+shaneiqra926@users.noreply.github.com>
2019-12-13 17:47:37 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import io
+import random
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text="Pakistan's Urban Population of the year you have chosen should be {}".format(output))
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
